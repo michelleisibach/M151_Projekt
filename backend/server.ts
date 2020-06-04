de6296e7beee9683868d61cff61ce10313bcfb6a
michelleisibach <50678154+michelleisibach@users.noreply.github.com>
2020-06-04 19:21:24 +0200

    udpated server.ts
    Server is now listening on Port 8080

--- backend/server.ts
+++ backend/server.ts
@@ -2,3 +2,25 @@ import * as express from "express";
 import * as path from "path";
 import * as bodyParser from "body-parser";
 import * as expressSession from "express-session";
+
+const app = express();
+app.use(bodyParser.json());
+//generate Session
+app.use(expressSession({
+    secret: "cookie",
+    resave: false,
+    saveUnitialized: true
+}));
+//use default assets like stylesheet etc...
+app.use("/assets", express.static(path.join(__dirname, "/../frontend/assets")));
+app.use(express.json());
+
+//frontend
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/html/index.html'));
+});
+
+//serverlistening
+app.listen(8080, () => {
+    console.log("Server listening on Port 8080")
+});
