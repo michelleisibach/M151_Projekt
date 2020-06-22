c2a31ee3a55fb1bac03cd2b98ec90884c7133b5c
michelleisibach <50678154+michelleisibach@users.noreply.github.com>
2020-06-22 23:56:30 +0200

    added every page and created static content

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,8 @@ import * as bodyParser from "body-parser";
 import * as expressSession from "express-session";
 
 const app = express();
+var pgp = require("pg-promise")(/*options*/);
+var db = pgp("postgres://root:mypassword@localhost:5432/churchappdb")
 app.use(bodyParser.json());
 //generate Session
 app.use(expressSession({
@@ -17,6 +19,15 @@ app.use(express.json());
 
 //frontend
 app.get("/", (req, res) => {
+   
+ /*   db.one("SELECT $1 AS value", 123)
+                .then(function (data) {
+                        console.log("DATA:", data.value);
+    })
+    .catch(function (error) {
+        console.log("ERROR:", error);
+    });*/
+
     res.sendFile(path.join(__dirname + '/../frontend/html/index.html'));
 });
 
@@ -32,7 +43,20 @@ app.get("/messe", (req, res) => {
     res.sendFile(path.join(__dirname + '/../frontend/html/messe.html'));
 });
 
+app.get("/priests", (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/html/priests.html'));
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname+ '/../frontend/html/contact.html'));
+});
+
+app.get("/aboutus", (req, res) => {
+    res.sendFile(path.join(__dirname+ '/../frontend/html/aboutus.html'));
+});
+
+
 //serverlistening
-app.listen(8080, () => {
-    console.log("Server listening on Port 8080")
+app.listen(5000, () => {
+    console.log("Server listening on Port 5000")
 });
